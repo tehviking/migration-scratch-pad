5b884c3c4fb0f6b31774cdb593e70e1dfff65fdc
Brandon Hays <brandon.hays@gmail.com>
2015-02-21 17:12:50 -0600

    bring over components

---
+++ app/components/pixelate-image.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+var PixelateImageComponent = Ember.Component.extend({
+  value: 0,
+
+  performPixelation: Ember.observer("value", function() {
+    var amount = this.get("value") / 100;
+    if(!this.pixelate) {
+      this.pixelate = new Pixelate(this.$("img").get(0), {amount: amount});
+    } else {
+      this.pixelate.setAmount(amount).render();
+    }
+  }).on("didInsertElement"),
+
+  rerenderOnSrcChange: Ember.observer("src", function() {
+    var _this = this;
+    Ember.run.later(function() {
+      _this.pixelate = null;
+      _this.rerender();
+    }, 100);
+  })
+});
+
+export default PixelateImageComponent;
