d5fccc59857d78526bd045bee5184df089306b69
Brandon Hays <brandon.hays@gmail.com>
2015-02-21 18:33:36 -0600

    specs pass in chrome

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+
+var application;
+
+module('Acceptance: Index', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /index', function(assert) {
+  visit('/index');
+
+  andThen(function() {
+    assert.equal($(".spec-gallery-image").length, 8);
+  });
+});
--- tests/unit/utils/gravatar-url-from-email-test.js
+++ tests/unit/utils/gravatar-url-from-email-test.js
@@ -5,6 +5,6 @@ module('gravatarUrlFromEmail');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  var result = gravatarUrlFromEmail();
-  assert.ok(result);
+  var result = gravatarUrlFromEmail("brandon.hays@gmail.com", 400);
+  assert.equal(result, "http://gravatar.com/avatar/6fd16b1b6a307ca583526e2ec4dab52d?s=400");
 });
